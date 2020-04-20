bd48383193fd46d94c1606ece4bc388e78e0468b
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-04-21 00:22:55 +0200

    Add base environment class

---
+++ reinforcement_learning/environments/environment.py
@@ -0,0 +1,29 @@
+import gym
+
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class Environment(ABC):
+
+    _env: gym.Env
+
+    @abstractmethod
+    def run_step(self, action):
+        pass
+
+    @abstractmethod
+    def reset_env(self):
+        pass
+
+    @abstractmethod
+    def get_state_number(self) -> int:
+        pass
+
+    @abstractmethod
+    def get_action_number(self) -> int:
+        pass
+
+    @abstractmethod
+    def probability_distribution(self):
+        pass
