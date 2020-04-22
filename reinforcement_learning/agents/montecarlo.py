80ebf3e1d1e5d99f50f8129884d349e4843a8b6a
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-04-22 19:26:03 +0200

    Add every fisit implementation to MonteCarloAgent

--- reinforcement_learning/agents/montecarlo.py
+++ reinforcement_learning/agents/montecarlo.py
@@ -11,22 +11,27 @@ class MonteCarloAgent():
 
     _epsilon: int
     _gamma: int
+    _visit_update: str
+    _policy_method: str
     _returns: Dict
-    _action_value: Action_value
+    _Q: Action_value
     _policy: Policy
     _env: Environment
 
-    def __init__(self, epsilon, gamma, environment):
+    def __init__(self, epsilon, gamma, environment, visit_update="first", policy_method="on-policy"):
+        #Basic attribute
         self._env = environment
         n_states = self._env.get_state_number()
         n_actions = self._env.get_action_number()
-        self._action_value = Action_value(n_states, n_actions)
+        self._Q = Action_value(n_states, n_actions)
         self._policy = Policy(n_states, n_actions)
         self._returns = defaultdict(int)
-
         self._epsilon = epsilon
         self._gamma = gamma
 
+        #Flags
+        self._visit_update = visit_update
+        self._policy_method = policy_method
 
     def run_agent(self, n_episodes: int, n_tests: int, test_step: int):
         test_results = defaultdict(list)
@@ -64,19 +69,31 @@ class MonteCarloAgent():
             s_t, a_t, r_t = episode_trajectory[i]
             G = (G * self._gamma) + r_t #Update expected return
 
-            #First visit implementation
-            if not (s_t, a_t) in [(s[0], s[1]) for s in episode_trajectory[0:i]]:
-                self._returns[(s_t, a_t)] += 1
-                
-                #Update action-value table
-                self._action_value[s_t, a_t] += (1 / self._returns[(s_t, a_t)]) * (G - self._action_value[s_t, a_t])
-                #Update Policy
-                self._update_policy(s_t)
+            if self._visit_update == "first":
+                self._first_visit_update(episode_trajectory[0:i], G, s_t, a_t)
+            elif self._visit_update == "every":
+                self._every_visit_update(G, s_t, a_t)
+
         return
+
+    def _first_visit_update(self, episode_trajectory_part, G, s_t, a_t):
+        if not (s_t, a_t) in [(s[0], s[1]) for s in episode_trajectory_part]:
+            self._returns[(s_t, a_t)] += 1 
+            #Update action-value table
+            self._Q[s_t, a_t] += (1 / self._returns[(s_t, a_t)]) * (G - self._Q[s_t, a_t])
+            #Update Policy
+            self._update_policy(s_t)
+
+    def _every_visit_update(self, G, s_t, a_t):
+            self._returns[(s_t, a_t)] += 1
+            #Update action-value table
+            self._Q[s_t, a_t] += (1 / self._returns[(s_t, a_t)]) * (G - self._Q[s_t, a_t])
+            #Update Policy
+            self._update_policy(s_t)
 
     def _update_policy(self, state_t) -> None:
         # Avoid choosing always the first move in case policy has the same value
-        indices = [i for i, x in enumerate(self._action_value[state_t]) if x == max(self._action_value[state_t])]
+        indices = [i for i, x in enumerate(self._Q[state_t]) if x == max(self._Q[state_t])]
         a_star = np.random.choice(indices)
 
         n_actions = self._policy.get_n_actions(state_t)
