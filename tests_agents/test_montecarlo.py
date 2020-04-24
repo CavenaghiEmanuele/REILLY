132e561c3cb14b178c3d44df62e61f42a8dfa3ba
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-04-24 23:31:13 +0200

    Add test "every" in montecarlo tests

--- tests_agents/test_montecarlo.py
+++ tests_agents/test_montecarlo.py
@@ -5,5 +5,7 @@ def test_MonteCarlo_agent():
 
     env = rl.Frozen_Lake4x4()
     agent = rl.MonteCarloAgent(epsilon=0.03, gamma=0.99, environment=env, visit_update="first")
+    agent.run(n_episodes=100, n_tests=10, test_step=10)
 
+    agent = rl.MonteCarloAgent(epsilon=0.03, gamma=0.99, environment=env, visit_update="every")
     agent.run(n_episodes=100, n_tests=10, test_step=10)
