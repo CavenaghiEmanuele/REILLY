8e1db152b80b45b55295ef7daccac202b80cdae4
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-04-28 01:32:05 +0200

    Update doc conf file

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -17,9 +17,13 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+import reinforcement_learning
+from mock import Mock
+sys.modules['reinforcement_learning'] = Mock()
 
 
 # -- General configuration ------------------------------------------------
@@ -31,7 +35,8 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.coverage']
 
@@ -42,7 +47,7 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = 'index'
