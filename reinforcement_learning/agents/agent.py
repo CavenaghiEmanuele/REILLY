ef93138ac7b780376e5faf6836d5fa2880036fac
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-05-19 20:57:05 +0200

    Update agent

--- reinforcement_learning/agents/agent.py
+++ reinforcement_learning/agents/agent.py
@@ -6,7 +6,15 @@ class Agent(ABC, object):
     __slots__ = ['_alpha', '_gamma', '_epsilon', '_S', '_episode_ended', '_Q', '_env', '_policy']
 
     @abstractmethod
-    def _control(self):
+    def run(self, n_episodes: int, n_tests: int, test_step: int):
+        pass
+
+    @abstractmethod
+    def train(self, n_episodes: int) -> None:
+        pass
+
+    @abstractmethod
+    def test(self, n_tests: int):
         pass
 
     @abstractmethod
@@ -15,4 +23,8 @@ class Agent(ABC, object):
 
     @abstractmethod
     def run_step(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def _control(self):
         pass
