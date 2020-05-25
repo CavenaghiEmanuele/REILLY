6cd4462ab313927720370884a3cb3a20008b7c3f
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-05-25 21:51:42 +0200

    Add test for n step Expected Sarsa

--- tests/test_n_step_bootstrapping.py
+++ tests/test_n_step_bootstrapping.py
@@ -15,3 +15,19 @@ def test_n_step_sarsa_agent():
     session.add_agent(agent)
     session.reset_env()
     session.run(100, 10, 10)
+
+
+def test_n_step_expected_sarsa_agent():
+    env = rl.Frozen_Lake4x4()
+    session = rl.Session(env)
+    agent = rl.NStepExpectedSarsaAgent(
+        states_size=env.states_size(),
+        actions_size=env.actions_size(),
+        alpha=0.1,
+        epsilon=0.03,
+        gamma=0.99,
+        n_step=5
+    )
+    session.add_agent(agent)
+    session.reset_env()
+    session.run(100, 10, 10)
