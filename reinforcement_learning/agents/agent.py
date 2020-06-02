cc0c842be17a710b367f14609ec1a89c60affb48
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-06-03 00:25:43 +0200

    Fix _n_step attribute name

--- reinforcement_learning/agents/agent.py
+++ reinforcement_learning/agents/agent.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 
 class Agent(ABC, object):
 
-    __slots__ = ['_alpha', '_gamma', '_epsilon', 'n_step', '_S', '_episode_ended', '_Q', '_env', '_policy']
+    __slots__ = ['_alpha', '_gamma', '_epsilon', '_n_step', '_S', '_episode_ended', '_Q', '_env', '_policy']
 
     @abstractmethod
     def reset(self, env):
