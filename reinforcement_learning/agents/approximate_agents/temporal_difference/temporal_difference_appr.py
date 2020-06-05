26b00a90a03d0d2d99a85622abd66b3c77ceca47
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-06-05 23:58:00 +0200

    Fix Approximate agents superclasses

--- reinforcement_learning/agents/approximate_agents/temporal_difference/temporal_difference_appr.py
+++ reinforcement_learning/agents/approximate_agents/temporal_difference/temporal_difference_appr.py
@@ -10,7 +10,7 @@ class TemporalDiffernceAppr(Agent, object):
 
     __slots__ = ["_Q_estimator"]
 
-    def __init__(self, alpha, epsilon, gamma, feature_dims, num_tilings, tiling_offset, tiles_dims):
+    def __init__(self, alpha, epsilon, gamma, feature_dims, num_tilings, tiling_offset=None, tiles_dims=None):
         # self._policy  -> Approximante agents don't have policy but approximate it
         self._alpha = alpha
         self._epsilon = epsilon
