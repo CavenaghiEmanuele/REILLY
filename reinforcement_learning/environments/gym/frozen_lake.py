fc7623c522d6b17298a09f4723241a94b05093c8
AlessioZanga <alessio.zanga@outlook.it>
2020-06-05 15:35:49 +0200

    Change to single quotes

--- reinforcement_learning/environments/gym/frozen_lake.py
+++ reinforcement_learning/environments/gym/frozen_lake.py
@@ -7,7 +7,7 @@ class Frozen_Lake4x4(GymEnvironment):
 
     def __init__(self, slippery=True):
         if slippery:
-            self._env = gym.make("FrozenLake-v0")
+            self._env = gym.make('FrozenLake-v0')
         else:
             gym.register(
                 id='FrozenLakeNotSlippery4x4-v0',
@@ -15,17 +15,17 @@ class Frozen_Lake4x4(GymEnvironment):
                 kwargs={'map_name': '4x4', 'is_slippery': False},
                 max_episode_steps=1000,
             )
-            self._env = gym.make("FrozenLakeNotSlippery4x4-v0")
+            self._env = gym.make('FrozenLakeNotSlippery4x4-v0')
         self.reset()
 
-    # If mod flag is "test" return additional dict with environment tests result
+    # If mod flag is 'test' return additional dict with environment tests result
     def run_step(self, action, *args, **kwargs):
         next_state, reward, done, _ = self._env.step(action)
 
-        if kwargs['mode'] == "test":
-            info = {"wins": 0}
+        if kwargs['mode'] == 'test':
+            info = {'wins': 0}
             if done and reward == 1:
-                info["wins"] = 1
+                info['wins'] = 1
             return next_state, reward, done, info
 
         return next_state, reward, done, _
@@ -35,7 +35,7 @@ class Frozen_Lake8x8(GymEnvironment):
 
     def __init__(self, slippery=True):
         if slippery:
-            self._env = gym.make("FrozenLake8x8-v0")
+            self._env = gym.make('FrozenLake8x8-v0')
         else:
             gym.register(
                 id='FrozenLakeNotSlippery8x8-v0',
@@ -43,17 +43,17 @@ class Frozen_Lake8x8(GymEnvironment):
                 kwargs={'map_name': '4x4', 'is_slippery': False},
                 max_episode_steps=1000,
             )
-            self._env = gym.make("FrozenLakeNotSlippery8x8-v0")
+            self._env = gym.make('FrozenLakeNotSlippery8x8-v0')
         self.reset()
 
-    # If mod flag is "test" return additional dict with environment tests result
+    # If mod flag is 'test' return additional dict with environment tests result
     def run_step(self, action, *args, **kwargs):
         next_state, reward, done, _ = self._env.step(action)
 
-        if kwargs['mode'] == "test":
-            info = {"wins": 0}
+        if kwargs['mode'] == 'test':
+            info = {'wins': 0}
             if done and reward == 1:
-                info["wins"] = 1
+                info['wins'] = 1
             return next_state, reward, done, info
 
         return next_state, reward, done, _
--- reinforcement_learning/environments/gym/taxi.py
+++ reinforcement_learning/environments/gym/taxi.py
@@ -6,17 +6,17 @@ from .abstract_gym import GymEnvironment
 class Taxi(GymEnvironment):
 
     def __init__(self):
-        self._env = gym.make("Taxi-v3")
+        self._env = gym.make('Taxi-v3')
         self.reset()
 
-    # If mod flag is "test" return additional dict with environment tests result
+    # If mod flag is 'test' return additional dict with environment tests result
     def run_step(self, action, *args, **kwargs):
         next_state, reward, done, _ = self._env.step(action)
 
-        if kwargs['mode'] == "test":
-            info = {"return_sum": reward, "wins": 0}
+        if kwargs['mode'] == 'test':
+            info = {'return_sum': reward, 'wins': 0}
             if done and reward == 20:
-                info["wins"] = 1                
+                info['wins'] = 1                
             return next_state, reward, done, info
 
         return next_state, reward, done, _
