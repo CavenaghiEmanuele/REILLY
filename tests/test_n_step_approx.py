88b9ee1acde6ca9c3e9988cf72ea43ce4536d676
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-06-05 23:57:15 +0200

    Add tests for approximate agents

--- tests/test_n_step_approx.py
+++ tests/test_n_step_approx.py
@@ -4,7 +4,7 @@ import reinforcement_learning as rl
 def test_n_step_sarsa_appr_agent():
     env = rl.Frozen_Lake4x4()
     session = rl.Session(env)
-    agent = rl.NStepSarsaApproximateAgent(
+    agent1 = rl.NStepSarsaApproximateAgent(
         alpha=0.1,
         epsilon=0.03,
         gamma=0.99,
@@ -14,7 +14,29 @@ def test_n_step_sarsa_appr_agent():
         tiling_offset=[1],
         tiles_dims=[1]
     )
-    session.add_agent(agent)
+    
+    agent2 = rl.NStepSarsaApproximateAgent(
+        alpha=0.1,
+        epsilon=0.03,
+        gamma=0.99,
+        n_step=5,
+        feature_dims=1,
+        num_tilings=2,
+        tiling_offset=1,
+        tiles_dims=1
+    )
+    
+    agent3 = rl.NStepSarsaApproximateAgent(
+        alpha=0.1,
+        epsilon=0.03,
+        gamma=0.99,
+        n_step=5,
+        feature_dims=1,
+        num_tilings=2
+    )
+    session.add_agent(agent1)
+    session.add_agent(agent2)
+    session.add_agent(agent3)
     session.reset_env()
     session.run(100, 10, 10)
     
--- tests/test_temporal_difference_appr.py
+++ tests/test_temporal_difference_appr.py
@@ -4,7 +4,7 @@ import reinforcement_learning as rl
 def test_sarsa_appr_agent():
     env = rl.Frozen_Lake4x4()
     session = rl.Session(env)
-    agent = rl.SarsaApproximateAgent(
+    agent1 = rl.SarsaApproximateAgent(
         alpha=0.1,
         epsilon=0.03,
         gamma=0.99,
@@ -13,7 +13,24 @@ def test_sarsa_appr_agent():
         tiling_offset=[1],
         tiles_dims=[1]
     )
-    session.add_agent(agent)
+    agent2 = rl.SarsaApproximateAgent(
+        alpha=0.1,
+        epsilon=0.03,
+        gamma=0.99,
+        feature_dims = 1,
+        num_tilings=2,
+        tiling_offset=1,
+        tiles_dims=1
+    )
+    agent3 = rl.SarsaApproximateAgent(
+        alpha=0.1,
+        epsilon=0.03,
+        gamma=0.99,
+        feature_dims = 1,
+        num_tilings=2
+    )
+    session.add_agent(agent1)
+    session.add_agent(agent2)
+    session.add_agent(agent3)
     session.reset_env()
     session.run(100, 10, 10)
-    
