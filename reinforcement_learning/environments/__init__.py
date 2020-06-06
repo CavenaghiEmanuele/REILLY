df3377398168cb7e0f0260faf7d251dfbce9772c
AlessioZanga <alessio.zanga@outlook.it>
2020-06-06 15:23:17 +0200

    Add ENVIRONMENT global list

--- reinforcement_learning/environments/__init__.py
+++ reinforcement_learning/environments/__init__.py
@@ -1,3 +1,15 @@
 from .environment import Environment
 from .gym import Taxi, Frozen_Lake4x4, Frozen_Lake8x8, MountainCar
 from .text import TextEnvironment, TextNeighbor
+
+
+def ENVIRONMENTS():
+    import sys, inspect
+    module = sys.modules[__name__]
+    return [
+        value
+        for _, value in module.__dict__.items()
+        if inspect.isclass(value) \
+            and issubclass(value, Environment) \
+            and value != Environment
+    ]
