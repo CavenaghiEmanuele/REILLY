9e47e5fe8d31c463bec78e62a92d4ba2fed8fb1b
AlessioZanga <alessio.zanga@outlook.it>
2020-06-06 15:23:49 +0200

    Add SARSA test for each environment

--- tests/test_temporal_difference.py
+++ tests/test_temporal_difference.py
@@ -17,18 +17,20 @@ def test_QLearning_agent():
 
 
 def test_Sarsa_agent():
-    env = rl.Frozen_Lake4x4()
-    session = rl.Session(env)
-    agent = rl.SarsaAgent(
-        states_size=env.states_size,
-        actions_size=env.actions_size,
-        alpha=0.1,
-        epsilon=0.03,
-        gamma=0.99
-    )
-    session.add_agent(agent)
-    session.reset_env()
-    session.run(100, 10, 10)
+    for E in rl.ENVIRONMENTS():
+        env = E()
+        env = rl.Frozen_Lake4x4()
+        session = rl.Session(env)
+        agent = rl.SarsaAgent(
+            states_size=env.states_size,
+            actions_size=env.actions_size,
+            alpha=0.1,
+            epsilon=0.03,
+            gamma=0.99
+        )
+        session.add_agent(agent)
+        session.reset_env()
+        session.run(100, 10, 10)
 
 
 def test_Expected_Sarsa_agent():
