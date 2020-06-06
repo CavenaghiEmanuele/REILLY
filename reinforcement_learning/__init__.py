0d52afd35c6eb9c5cebd61398a886507a9119962
AlessioZanga <alessio.zanga@outlook.it>
2020-06-06 15:23:06 +0200

    Update MountainCar env structure

--- reinforcement_learning/__init__.py
+++ reinforcement_learning/__init__.py
@@ -1,14 +1,14 @@
 from .agents import (
-    MonteCarloAgent, 
+    MonteCarloAgent,
     TemporalDifference, SarsaAgent, QLearningAgent, ExpectedSarsaAgent,
     DoubleTemporalDifference, DoubleSarsaAgent, DoubleQLearningAgent, DoubleExpectedSarsaAgent,
     NStep, NStepSarsaAgent, NStepExpectedSarsaAgent,
-    
+
     TileCoding, Tiling, QEstimator,
     TemporalDiffernceAppr, SarsaApproximateAgent,
     NStepAppr, NStepSarsaApproximateAgent
-    )
-from .environments import Environment, Taxi, Frozen_Lake4x4, Frozen_Lake8x8, MountainCar, TextEnvironment, TextNeighbor
-from .sessions import Session
-from .structures import StateValue, ActionValue, Policy
+)
+from .environments import *
+from .sessions import *
+from .structures import *
 from .utils import plot
--- reinforcement_learning/environments/gym/mountain_car.py
+++ reinforcement_learning/environments/gym/mountain_car.py
@@ -7,21 +7,9 @@ class MountainCar(Environment):
 
     def __init__(self):
         self._env = gym.make("MountainCar-v0")
-        self.reset_env()
+        self.reset()
 
-    def render(self):
-        return self._env.render()
-
-    # If mod flag is "test" return additional dict with environment tests result
-    def run_step(self, action, *args, **kwargs):
-        next_state, reward, done, _ = self._env.step(action)
-        if kwargs['mode'] == "test":
-            return next_state, reward, done, {"return_sum": reward}
-        return next_state, reward, done, _
-
-    def reset_env(self):
-        return self._env.reset()
-
+    @property
     def states_size(self):
         '''
         Discretize the state space for tabular agents. One simple way in which
@@ -31,8 +19,23 @@ class MountainCar(Environment):
         '''
         return 285
 
+    @property
     def actions_size(self):
         return self._env.action_space.n
 
+    def render(self):
+        return self._env.render()
+
+    def reset(self) -> int:
+        return self._env.reset()
+    
+    # If mod flag is "test" return additional dict with environment tests result
+    def run_step(self, action, *args, **kwargs):
+        next_state, reward, done, _ = self._env.step(action)
+        if kwargs['mode'] == "test":
+            return next_state, reward, done, {"return_sum": reward}
+        return next_state, reward, done, _
+
+    @property
     def probability_distribution(self):
         return self._env.env.P
