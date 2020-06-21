5b080609e992b56cdaf3048a0db0a618b033a0da
AlessioZanga <alessio.zanga@outlook.it>
2020-06-22 00:46:57 +0200

    Add PyBindSession adapter

--- reinforcement_learning/backend/__init__.py
+++ reinforcement_learning/backend/__init__.py
@@ -4,7 +4,7 @@ build = os.path.join(os.path.dirname(__file__), 'build')
 if not os.path.exists(build):
     os.mkdir(build)
 
-success = os.system('cd ' + build + ' && cmake .. && make clean && make')
+success = os.system('cd ' + build + ' && cmake .. && make')
 if (success != 0):
     raise RuntimeError('Backend cross-compile failed.')
 
--- reinforcement_learning/sessions/__init__.py
+++ reinforcement_learning/sessions/__init__.py
@@ -1,3 +1,4 @@
 from .session import Session
 from .single_train_session import SingleTrainSession
 from .joint_train_session import JointTrainSession
+from .pybind_session import PyBindSession
