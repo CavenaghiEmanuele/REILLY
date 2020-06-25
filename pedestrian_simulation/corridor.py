e42930a31687cea2cbcb2729c72f4a1c033cb982
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-06-25 16:34:46 +0200

    Fix pedestrian environment wall

--- pedestrian_simulation/corridor.py
+++ pedestrian_simulation/corridor.py
@@ -5,16 +5,16 @@ def corridor():
 
     # 25 x 8
     ENV = rl.TextEnvironment(
-        text=   "#########################\n" +
-                "S                       X\n" +
-                "S                       X\n" +
-                "S                       X\n" +
-                "S                       X\n" +
-                "S                       X\n" +
-                "S                       X\n" +
-                "S                       X\n" +
-                "S                       X\n" +
-                "#########################\n",
+        text=   "##########################\n" +
+                "#S                       X\n" +
+                "#S                       X\n" +
+                "#S                       X\n" +
+                "#S                       X\n" +
+                "#S                       X\n" +
+                "#S                       X\n" +
+                "#S                       X\n" +
+                "#S                       X\n" +
+                "##########################\n",
 
         max_steps=100,
         neighbor=rl.TextNeighbor.NEUMANN,
