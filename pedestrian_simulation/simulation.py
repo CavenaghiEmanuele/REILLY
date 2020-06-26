241e403e1be9419c8dea80a5008f5bcd29162fea
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-06-26 23:41:02 +0200

    Update simulation in pedestrian simulation

--- pedestrian_simulation/simulation.py
+++ pedestrian_simulation/simulation.py
@@ -1,17 +1,60 @@
+import pedestrian_simulation as ps
 import pandas as pd
+from copy import deepcopy
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..')))
+
 import reinforcement_learning as rl
-import pedestrian_simulation as ps
 
 
-def simulate(env, n_agents: int, start_step:int = 1):
-    
+def simulate(env, n_agents: int, start_step: int = 1):
     single_train_session = rl.SingleTrainSession(env, start_step=start_step)
-    
+
     for _ in range(n_agents):
         agent = rl.SarsaLambdaAgent(
+            alpha=0.1,
+            epsilon=0.3,
+            gamma=0.99,
+            lambd=0.98,
+            epsilon_decay=0.99,
+            trace_type="replacing",
+            feature_dims=2,
+            num_tilings=4,
+            tiles_size=[4, 4],
+        )
+        single_train_session.add_agent(agent)
+
+    results = single_train_session.run(500, 50, 50)
+    rl.plot(results)
+
+
+def duplicated_simulation(env, n_train_agents, n_duplication: int, start_step: int = 1):
+
+    single_train_session = rl.SingleTrainSession(env, start_step=start_step)
+    trained_agents = []
+
+    for _ in range(n_train_agents):
+        agent = rl.SarsaLambdaAgent(
+            alpha=0.1,
+            epsilon=0.3,
+            gamma=0.99,
+            lambd=0.98,
+            epsilon_decay=0.99,
+            trace_type="replacing",
+            feature_dims=2,
+            num_tilings=4,
+            tiles_size=[4, 4],
+        )
+        single_train_session.add_agent(agent)
+        trained_agents.append(agent)
+    
+    single_train_session.run(500, 500, 1)
+
+    for agent in trained_agents:
+        for _ in range(n_duplication):
+            copy_agent = rl.SarsaLambdaAgent(
                 alpha=0.1,
                 epsilon=0.3,
                 gamma=0.99,
@@ -20,12 +63,14 @@ def simulate(env, n_agents: int, start_step:int = 1):
                 trace_type="replacing",
                 feature_dims=2,
                 num_tilings=4,
-                tiles_size=[4,4],
+                tiles_size=[4, 4],
             )
-        single_train_session.add_agent(agent)
+            copy_agent._Q_estimator = deepcopy(agent._Q_estimator)
+            single_train_session.add_agent(copy_agent)
 
-    results = single_train_session.run(500, 50, 50)
+    results = single_train_session.run(10, 1, 50)
     rl.plot(results)
+    
 
 def travelling_time(results):
     groupby = results.drop(['return_sum', 'wins', 'time', 'distance'], axis=1).\
@@ -39,7 +84,10 @@ def travelling_time(results):
         for _, row in scatter.iterrows()
     ]
 
+
 if __name__ == "__main__":
+
+    env = ps.three_room()
+    #simulate(env, n_agents=20, start_step=4)
+    duplicated_simulation(env, n_train_agents=20, n_duplication=100, start_step=250)
     
-    env = ps.T()
-    simulate(env, n_agents=20, start_step=4)
