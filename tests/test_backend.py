b7b1a00bfdb28de4cd0ae6e0060674e266633627
AlessioZanga <alessio.zanga@outlook.it>
2020-06-30 12:29:22 +0200

    Add tests

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -187,3 +187,35 @@ def test_PrioritizedSweeping():
     )
     session.add_agent(agent)
     session.run(100, 10, 10)
+
+def test_SemiGradientSarsa():
+    env = rl.Frozen_Lake4x4()
+    session = rl.PyBindSession(env)
+    agent = rl.backend.SemiGradientExpectedSarsa(
+        actions=env.actions,
+        alpha=0.1,
+        epsilon=0.01,
+        gamma=0.99,
+        features=1,
+        tilings=2,
+        tilings_offset=[4, 4],
+        tile_size=[4, 4]
+    )
+    session.add_agent(agent)
+    session.run(100, 10, 10)
+
+def test_SemiGradientExpectedSarsa():
+    env = rl.Frozen_Lake4x4()
+    session = rl.PyBindSession(env)
+    agent = rl.backend.SemiGradientExpectedSarsa(
+        actions=env.actions,
+        alpha=0.1,
+        epsilon=0.01,
+        gamma=0.99,
+        features=1,
+        tilings=2,
+        tilings_offset=[4, 4],
+        tile_size=[4, 4]
+    )
+    session.add_agent(agent)
+    session.run(100, 10, 10)
