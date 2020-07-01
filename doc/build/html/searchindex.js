b94c4c0a21c8e33acbc2262ac748a46b5d81d954
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-07-01 15:31:21 +0200

    Delete docs build directory

--- doc/build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to Reinforcement Learning Library\u2019s documentation!"],titleterms:{document:0,indic:0,learn:0,librari:0,reinforc:0,tabl:0,welcom:0}})
