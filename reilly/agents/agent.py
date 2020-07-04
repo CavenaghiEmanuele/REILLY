219d0c3d9a77674b4fc348e98648054a20e8dec7
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-07-04 11:47:40 +0200

    Update agent interface

--- reilly/agents/agent.py
+++ reilly/agents/agent.py
@@ -8,25 +8,19 @@ class Agent(ABC, object):
 
     __slots__ = ['_alpha', '_gamma', '_epsilon', '_e_decay', '_n_step',
                  '_A', '_S', '_episode_ended', '_actions',
-                 '_Q', '_policy']
+                ]
 
     def get_action(self):
         return self._A
-    
-    def _select_action(self, weights: List) -> None:
-        return np.random.choice(range(self._actions), p=weights)
 
-    def _e_greedy_policy(self, weights: List) -> None:
-        # Avoid choosing always the first move in case policy has the same value
-        indices = [i for i, x in enumerate(weights) if x == max(weights)]
-        A_star = np.random.choice(indices)
-
-        for A in range(self._actions):
-            if A == A_star:
-                self._policy[self._S, A] = 1 - self._epsilon + (self._epsilon / self._actions)
-            else:
-                self._policy[self._S, A] = self._epsilon / self._actions
+    @abstractmethod
+    def update(self, n_S: int, R: float, done: bool, *args, **kwargs):
+        pass
 
     @abstractmethod
     def reset(self, env, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def _select_action(self, weights: List) -> None:
         pass
